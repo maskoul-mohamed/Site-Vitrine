3aa13a94ab60e626852431e8d4b1511eac450638
tebbaa-adnane <razorzeroday@gmail.com>
2021-11-24 10:38:19 +0100

    update.

--- server.py
+++
@@ -1,7 +0,0 @@
-import http.server
-import socketserver
-PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("192.168.1.123", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
